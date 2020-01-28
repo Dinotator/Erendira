4ab82be7ed204bacb22461057b73f9ef60cc04e0
Dinotator <56325558+Dinotator@users.noreply.github.com>
2020-01-28 09:41:38 -0600

    Add files via upload

---
+++ Erendira/src/erendira/Erendira.java
@@ -0,0 +1,16 @@
+package erendira;
+
+
+public class Erendira {
+
+    
+    public static void main(String[] args) {
+        Index i = new Index();
+        i.setTitle("Inicio");
+        i.setResizable(false);
+        i.setLocationRelativeTo(i);
+        i.setVisible(true);
+        
+    }
+    
+}
